0d8a5e5de6ce4ebc9e00236cb4185a6fbf3ede94
James Firth <james.firth@norimaconsulting.com>
2021-11-30 12:37:37 -0600

    format(booleaninput): prettier auto-format through VS Code

--- src/scalarComponents/primereact/BooleanInput.tsx
+++ src/scalarComponents/primereact/BooleanInput.tsx
@@ -1,16 +1,16 @@
-import React, { FunctionComponent } from 'react'
-import { bs } from '@tesseractcollective/react-graphql'
+import { Checkbox, CheckboxProps } from 'primereact/checkbox';
+import React, { FunctionComponent } from 'react';
+import { useController } from 'react-hook-form';
 import { ScalarComponentPropsBase } from '../../types/generic';
-import { Checkbox, CheckboxProps } from 'primereact/checkbox'
-import { useController } from 'react-hook-form'
 
 export interface IBooleanInputProps extends ScalarComponentPropsBase {
-  inputProps?: CheckboxProps
+  inputProps?: CheckboxProps;
 }
 
 const BooleanInput: FunctionComponent<IBooleanInputProps> =
   function BooleanInput(props) {
-    const { fieldInfo, control, inputProps, rules, ...passthroughProps } = props
+    const { fieldInfo, control, inputProps, rules, ...passthroughProps } =
+      props;
     const {
       field: { ref, onChange, ...inputControllerProps },
       fieldState: { invalid, isTouched, isDirty, error },
@@ -20,7 +20,7 @@ const BooleanInput: FunctionComponent<IBooleanInputProps> =
       control,
       rules,
       defaultValue: '',
-    })
+    });
 
     return (
       <div>
@@ -28,15 +28,17 @@ const BooleanInput: FunctionComponent<IBooleanInputProps> =
           id={'ff-' + fieldInfo.name}
           ref={ref}
           onChange={(e) => {
-            onChange({target: {value: e.value}})
+            onChange({ target: { value: e.value } });
           }}
           {...inputControllerProps}
           {...inputProps}
           {...passthroughProps}
         />
-        <small id="username2-help" className="p-error p-d-block">{error?.message || error?.type}</small>
+        <small id="username2-help" className="p-error p-d-block">
+          {error?.message || error?.type}
+        </small>
       </div>
-    )
-  }
+    );
+  };
 
-export default BooleanInput
+export default BooleanInput;
