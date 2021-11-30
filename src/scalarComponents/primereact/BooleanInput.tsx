5701769846c70f745b9fb85a1ea7ed048fe3a8a5
James Firth <james.firth@norimaconsulting.com>
2021-11-30 12:37:37 -0600

    fix/feat(booleaninput): extra styling options and use boolean state not string

--- src/scalarComponents/primereact/BooleanInput.tsx
+++ src/scalarComponents/primereact/BooleanInput.tsx
@@ -4,13 +4,44 @@ import { useController } from 'react-hook-form';
 import { ScalarComponentPropsBase } from '../../types/generic';
 
 export interface IBooleanInputProps extends ScalarComponentPropsBase {
+  /**
+   * Props
+   */
   inputProps?: CheckboxProps;
+  /**
+   * CSS Style objects per sub-component for detailed styling
+   */
+  styles?: {
+    containerStyle?: any | Readonly<any>;
+    checkboxLabelWrapperStyle?: any | Readonly<any>;
+    labelStyle?: any | Readonly<any>;
+    checkboxStyle?: any | Readonly<any>;
+    errorStyle?: any | Readonly<any>;
+  };
+
+  /**
+   * Styling via class names like tailwind or prime flex
+   */
+  checkboxLabelWrapperClassName?: string;
+  containerClassName?: string;
+  /**
+   * Whether to put the label above, matching float labels for other prime react components (true)
+   * Or to put it to the side (false)
+   * Default: true
+   */
+  floatLabel?: boolean;
 }
 
 const BooleanInput: FunctionComponent<IBooleanInputProps> =
   function BooleanInput(props) {
-    const { fieldInfo, control, inputProps, rules, ...passthroughProps } =
-      props;
+    const {
+      fieldInfo,
+      control,
+      inputProps,
+      rules,
+      floatLabel = true,
+      ...passthroughProps
+    } = props;
     const {
       field: { ref, onChange, ...inputControllerProps },
       fieldState: { invalid, isTouched, isDirty, error },
@@ -19,22 +50,53 @@ const BooleanInput: FunctionComponent<IBooleanInputProps> =
       name: fieldInfo.name,
       control,
       rules,
-      defaultValue: '',
+      defaultValue: false,
     });
 
     return (
-      <div>
-        <Checkbox
-          id={'ff-' + fieldInfo.name}
-          ref={ref}
-          onChange={(e) => {
-            onChange({ target: { value: e.value } });
-          }}
-          {...inputControllerProps}
-          {...inputProps}
-          {...passthroughProps}
-        />
-        <small id="username2-help" className="p-error p-d-block">
+      <div
+        className={`p-field-checkbox ${passthroughProps?.containerClassName}`}
+        style={passthroughProps?.styles?.containerStyle}
+      >
+        <div
+          className={`${passthroughProps?.checkboxLabelWrapperClassName}`}
+          style={passthroughProps?.styles?.checkboxLabelWrapperStyle}
+        >
+          <Checkbox
+            id={'ff-' + fieldInfo.name}
+            ref={ref}
+            checked={inputControllerProps?.value}
+            {...inputProps}
+            onChange={(e) => {
+              onChange(e.checked);
+            }}
+            {...inputControllerProps}
+            {...passthroughProps}
+            style={passthroughProps?.styles?.checkboxStyle ?? {}}
+          />
+          <label
+            htmlFor={'ff-' + fieldInfo.name}
+            style={
+              passthroughProps?.styles?.labelStyle ?? floatLabel
+                ? {
+                    position: 'relative',
+                    top: '-1.6rem',
+                    fontSize: '12px',
+                    color: '#6c757d',
+                  }
+                : {
+                    paddingLeft: '1rem',
+                  }
+            }
+          >
+            {fieldInfo?.label ?? fieldInfo?.name}
+          </label>
+        </div>
+        <small
+          id="username2-help"
+          className="p-error p-d-block"
+          style={passthroughProps?.styles?.errorStyle}
+        >
           {error?.message || error?.type}
         </small>
       </div>
